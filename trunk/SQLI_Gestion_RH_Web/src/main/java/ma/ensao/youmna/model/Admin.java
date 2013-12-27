023192d5d717feb75ba0d557195c9b586ef43a87
mossnaj@gmail.com <mossnaj@gmail.com@120e1ad8-7710-a5a5-45f0-4159f9656e84>
2013-12-27 20:23:15 +0000

    Integration of hibernate (Postgresql)

---
+++ trunk/SQLI_Gestion_RH_Web/src/main/java/ma/ensao/youmna/model/Admin.java
@@ -0,0 +1,39 @@
+package ma.ensao.youmna.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+	
+	@Id @GeneratedValue
+	@Column(name = "id", nullable = false)
+	private Long id;
+	@Column(name = "user_name", nullable = false)
+	private String username;
+	@Column(name = "password", nullable = false)
+	private String password;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
