64ca992e06481c44239f271b0d19a03487ddd156
yasser.ouaftouh@gmail.com <yasser.ouaftouh@gmail.com@120e1ad8-7710-a5a5-45f0-4159f9656e84>
2014-01-08 20:12:50 +0000

    + CreateUser (jQuery) + Model Re-architecture (ManyToOne)

--- SQLI_Gestion_RH_Web/src/main/java/ma/ensao/youmna/model/Diplome.java
+++ SQLI_Gestion_RH_Web/src/main/java/ma/ensao/youmna/model/Diplome.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +25,9 @@ public class Diplome {
 	private String diplomeType;
 	@Column(name = "promotion", nullable = false)
 	private Integer promotion;
+	@ManyToOne
+	@JoinColumn(name="matricule")
+	private Collaborateur collaborateur;
 	public String getId() {
 		return id;
 	}
